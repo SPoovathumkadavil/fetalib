2ae2012fdab9f7dcd6019d60d010bb7a0642f9c9
Sally <spoovathumkadavil16@claytonschools.net>
2024-09-01 06:56:53 -0500

    build testing

--- test/src/directory_tests.cpp
+++ test/src/directory_tests.cpp
@@ -8,12 +8,12 @@ TEST(DirTest, EnsureTestProjDirGetter)
 {
   feta::directories fetadirs("test_project", "", true);
   auto pdir = fetadirs.proj_dirs();
-  ASSERT_EQ(std::string(pdir.lib.c_str()), std::string("library"));
+  EXPECT_EQ(std::string(pdir.lib.c_str()), std::string("library"));
 }
 
 TEST(DirTest, EnsureNoTestThrowsWithIncorrectLoc)
 {
-  ASSERT_THROW({
+  EXPECT_THROW({
     feta::directories fetadirs("test_project", "test/resources/test.json");
   }, std::invalid_argument);
 }
@@ -24,5 +24,5 @@ TEST(DirTest, EnsureNoTestProjDirGetter)
   feta::JsonReader reader(fetadirs.loc_file_location());
   auto pdir = fetadirs.proj_dirs();
   std::filesystem::path test_path = std::filesystem::path(reader.read_l1_entry("library")) / std::filesystem::path(fetadirs.project_name());
-  ASSERT_EQ(pdir.lib, test_path);
+  EXPECT_EQ(pdir.lib, test_path);
 }
