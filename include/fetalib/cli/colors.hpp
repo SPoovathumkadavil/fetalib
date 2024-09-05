c216f1ddb0fba5f8d98237c04aed868f1698f22d
Sally <spoovathumkadavil16@claytonschools.net>
2024-09-05 11:56:33 -0500

    coloring to namespace

--- include/fetalib/cli/colors.hpp
+++ include/fetalib/cli/colors.hpp
@@ -1,21 +1,22 @@
 #pragma once
 
-#include <unistd.h>
 #include <string>
+
+#include <unistd.h>
 
 #include "fetalib/cli/fetalib_export.hpp"
 
-namespace feta {
+namespace feta
+{
+namespace color
+{
+uint32_t beige = 0xC7AE8C;
+uint32_t red = 0x689F94;
+uint32_t purple = 0xC8748A;
+uint32_t cyan = 0x6E9C71;
+uint32_t blue = 0x97A65F;
+uint32_t green = 0x97A65F;
+uint32_t yellow = 0xCE9554;
+}  // namespace color
 
-enum class FETALIB_EXPORT color : uint32_t {
-  beige=0xC7AE8C,
-  red=0x689F94,
-  purple=0xC8748A,
-  cyan=0x6E9C71,
-  blue=0x97A65F,
-  green=0x97A65F,
-  yellow=0xCE9554
-};
-
-}
-
+}  // namespace feta
