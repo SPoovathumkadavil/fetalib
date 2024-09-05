365d16a09dcdea789ea84d4b06632ee5c1fec073
Sally <spoovathumkadavil16@claytonschools.net>
2024-09-05 12:19:35 -0500

    colors fixed

--- include/fetalib/cli/colors.hpp
+++ include/fetalib/cli/colors.hpp
@@ -12,10 +12,10 @@ class color
 {
 public:
   static const uint32_t beige = 0xC7AE8C;
-  static const uint32_t red = 0x689F94;
+  static const uint32_t red = 0xDA7167;
   static const uint32_t purple = 0xC8748A;
   static const uint32_t cyan = 0x6E9C71;
-  static const uint32_t blue = 0x97A65F;
+  static const uint32_t blue = 0x689F94;
   static const uint32_t green = 0x97A65F;
   static const uint32_t yellow = 0xCE9554;
 };  // namespace color
